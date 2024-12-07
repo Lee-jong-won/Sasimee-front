771ce4e7d4836f610b7b7949dd5badad718c6fb9
Nahyun Kim <hedghog1004@naver.com>
2024-12-07 20:07:55 +0900

    ♻️ Refactor: 실험 그래픽 아이템 Column 안의 위젯들 분리
    - 이미지, 설명, 버튼 부분 분리
    
    Related to: #2

--- lib/screens/main/widgets/experiment_graphic_item.dart
+++ lib/screens/main/widgets/experiment_graphic_item.dart
@@ -33,67 +33,79 @@ class ExperimentGraphicItem extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Center(
-            child: SizedBox(
-              width: 80,
-              height: 80,
-              child: type == ExperimentType.SURVEY
-                  ? Image.asset('assets/images/graphics/img_survey.png')
-                  : Image.asset('assets/images/graphics/img_perform.png')
-            ),
-          ),
+          _graphicImage(),
           const SizedBox(height: 17,),
-          Text(type == ExperimentType.SURVEY
-              ? 'survey_description'.tr()
-              : 'perform_description'.tr(),
-            style: const TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.w600,
-                color: Colors.black
-            ),
-          ),
+          _description(),
           const SizedBox(height: 17,),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 10, horizontal: 16
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.2),
-                  spreadRadius: 1,
-                  blurRadius: 4,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: GestureDetector(
-              onTap: (){
-                //TODO: 실험 생성 화면으로 이동
-                print("실헝 생성, $type");
-              },
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(type == ExperimentType.SURVEY
-                        ? "survey_create".tr()
-                        : "perform_create".tr(),
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.black
-                      ),
-                    ),
-                  ),
-                  SvgIcons.arrowDotted
-                ],
-              ),
-            ),
+          _bottomButton()
+        ],
+      ),
+    );
+  }
+
+  Widget _graphicImage() {
+    return Center(
+      child: SizedBox(
+          width: 80,
+          height: 80,
+          child: type == ExperimentType.SURVEY
+              ? Image.asset('assets/images/graphics/img_survey.png')
+              : Image.asset('assets/images/graphics/img_perform.png')
+      ),
+    );
+  }
+
+  Widget _description() {
+    return Text(type == ExperimentType.SURVEY
+        ? 'survey_description'.tr()
+        : 'perform_description'.tr(),
+      style: const TextStyle(
+          fontSize: 13,
+          fontWeight: FontWeight.w600,
+          color: Colors.black
+      ),
+    );
+  }
+
+  Widget _bottomButton() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+          vertical: 10, horizontal: 16
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 4,
+            offset: const Offset(0, 3), // changes position of shadow
           ),
         ],
+      ),
+      child: GestureDetector(
+        onTap: (){
+          //TODO: 실험 생성 화면으로 이동
+          print("실헝 생성, $type");
+        },
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(type == ExperimentType.SURVEY
+                  ? "survey_create".tr()
+                  : "perform_create".tr(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.black
+                ),
+              ),
+            ),
+            SvgIcons.arrowDotted
+          ],
+        ),
       ),
     );
   }
