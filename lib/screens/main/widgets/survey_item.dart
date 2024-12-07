3832b8ac7263fd3ec8fe4158366efffc3e23d867
Nahyun Kim <hedghog1004@naver.com>
2024-12-08 02:05:13 +0900

    ♻️ Refactor: 설문형 아이템 풀 사이즈 옵션 설정
    - enum으로 기본(라운딩, 그림자 적용) 아이템과 화면을 꽉 채우는 아이템 구분
    
    Related to: #4

--- lib/screens/main/widgets/survey_item.dart
+++ lib/screens/main/widgets/survey_item.dart
@@ -3,26 +3,37 @@ import 'package:sasimee/widgets/tag_item.dart';
 
 import '../../../styles/color_styles.dart';
 
+enum ItemViewType { DEFAULT, FULL }
+
 class SurveyItem extends StatelessWidget {
   final String title; //TODO: Survey 데이터로 변경
+  final ItemViewType viewType;
 
-  const SurveyItem({super.key, required this.title});
+  const SurveyItem({
+    super.key,
+    this.viewType = ItemViewType.DEFAULT,
+    required this.title
+  });
 
   @override
   Widget build(BuildContext context) {
+    bool isDefaultView = viewType == ItemViewType.DEFAULT;
+
     return Container(
       width: double.infinity,
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 1,
-            blurRadius: 4,
-            offset: const Offset(0, 3), // changes position of shadow
-          ),
-        ],
+          color: Colors.white,
+          borderRadius: isDefaultView ? BorderRadius.circular(10) : null,
+          boxShadow: isDefaultView
+              ? [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 4,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  )
+                ]
+              : null
       ),
       padding: const EdgeInsets.all(15),
       child: Column(
