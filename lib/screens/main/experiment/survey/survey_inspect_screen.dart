6ae6a72accb950a18f88543a69affb279a5c2fbf
Nahyun Kim <hedghog1004@naver.com>
2024-12-14 17:49:12 +0900

    ✨ Feat: 설문 검수 화면에 다이얼로그 노출
    Related to: #8

--- lib/screens/main/experiment/survey/survey_inspect_screen.dart
+++ lib/screens/main/experiment/survey/survey_inspect_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:sasimee/widgets/outline_text_field.dart';
 
 import '../../../../styles/color_styles.dart';
+import '../../../../widgets/common_dialog.dart';
 
 class SurveyInspectScreen extends StatelessWidget {
   static String routeName = "/survey-inspect";
@@ -22,7 +23,7 @@ class SurveyInspectScreen extends StatelessWidget {
             _surveyInspectLayout(),
             Column(
               children: [
-                _inspectionButton(),
+                _inspectionButton(context),
                 const SizedBox(height: 139,),
               ],
             )
@@ -70,20 +71,53 @@ class SurveyInspectScreen extends StatelessWidget {
   }
 
   //TODO: 설문 검수 진행
-  Widget _inspectionButton() {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(vertical: 12),
-      decoration: BoxDecoration(
-        color: ColorStyles.primaryBlue,
-        borderRadius: BorderRadius.circular(50),
-      ),
-      child: Text("survey_do_inspect".tr(),
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.w600,
-          color: Colors.white,
+  Widget _inspectionButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        //TODO: 유효하지 않은 주소일 경우 다이얼로그 노출
+        showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (context) {
+              return CommonDialog(
+                  title: "survey_inspect_dialog_title".tr(),
+                  description: "survey_inspect_dialog_description".tr(),
+                  buttonWidgetList: [
+                    Expanded(
+                        child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(15.0),
+                        backgroundColor: ColorStyles.primaryBlue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                      child: Text("ok".tr(),
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                    )),
+                  ]);
+            });
+      },
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          color: ColorStyles.primaryBlue,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Text(
+          "survey_do_inspect".tr(),
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
         ),
       ),
     );
