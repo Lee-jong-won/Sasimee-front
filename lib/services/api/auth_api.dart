0483e617ef3d682a97701e628f7864aa9a7a7ee3
Nahyun Kim <hedghog1004@naver.com>
2024-12-16 15:55:33 +0900

    ✨ Feat: 로그인 API 연동
    - 기존 예시 model 및 repository 삭제
    - 토큰 저장 로직 구현
    
    Related to: #27

--- lib/services/api/auth_api.dart
+++ lib/services/api/auth_api.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:sasimee/models/response/auth/post_login_response.dart';
 
+import '../../models/request/auth/post_login_request.dart';
 import '../../utils/constants.dart';
 
 part 'auth_api.g.dart';
@@ -9,6 +11,13 @@ part 'auth_api.g.dart';
 abstract class AuthApi {
   factory AuthApi(Dio dio, {String baseUrl}) = _AuthApi;
 
+  // 로그인
+  @POST("/user/login")
+  Future<PostLoginResponse> postLogin(
+      @Body() PostLoginRequest postLoginRequest
+  );
+
+  //TODO: 토큰 재발급
   @PUT("/auth/token")
   Future<void> putNewAccessToken();
 }
