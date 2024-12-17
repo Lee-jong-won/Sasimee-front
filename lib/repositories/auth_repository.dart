30d4ad930a69e2b8a3db29c8e743f1780d8a83ca
KTAEHWA <tthwa23@gmail.com>
2024-12-18 01:00:16 +0900

    ✨ Feat: 회원가입 API 연동

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -1,18 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:logger/logger.dart';
+import 'package:sasimee/models/request/auth/post_email_send_request.dart';
+import 'package:sasimee/models/request/auth/post_email_verify_request.dart';
 import 'package:sasimee/models/request/auth/post_login_request.dart';
+import 'package:sasimee/models/request/auth/post_register_request.dart';
+import 'package:sasimee/models/response/auth/post_email_send_response.dart';
 import 'package:sasimee/models/response/auth/post_login_response.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sasimee/models/response/default_response.dart';
 
-import '../../services/data/shared_preferences_service.dart';
 import '../services/api/auth_api.dart';
 import '../services/data/secure_storage_service.dart';
 import '../services/network/dio_service.dart';
 import '../utils/constants.dart';
 import '../utils/logger.dart';
 
-class LoginRepository {
+class AuthRepository {
   final AuthApi _authApi = AuthApi(DioService().get());
   final Logger logger = LoggerUtils().get();
   // final SharedPreferences _sharedPreferences = SharedPreferencesService.get();
@@ -39,6 +42,65 @@ class LoginRepository {
       return loginResponse;
     } catch (e) {
       logger.e("Login Error", error: e);
+
+      if (e is DioException && e.response != null) {
+        return null;
+      }
+
+      return null;
+    }
+  }
+
+  /// 인증 메일 보내기
+  Future<PostEmailSendResponse?> sendAuthEmail(String email) async {
+    try {
+      final request = PostEmailSendRequest(
+        to: email,
+        subject: '인증 메일',
+        message: '사심이',
+      );
+
+      var response = await _authApi.send(request);
+      return response;
+    } catch (e) {
+      logger.e("Failed to send authentication email.", error: e);
+
+      if (e is DioException && e.response != null) {
+        return null;
+      }
+
+      return null;
+    }
+  }
+
+  /// 인증 코드 검증
+  Future<DefaultResponse?> verifyEmail(String email, String authNum) async {
+    try {
+      final request = PostEmailVerifyRequest(
+        email: email,
+        authNum: authNum,
+      );
+
+      var response = await _authApi.verify(request);
+      return response;
+    } catch (e) {
+      logger.e("Failed to verify email authentication.", error: e);
+
+      if (e is DioException && e.response != null) {
+        return null;
+      }
+
+      return null;
+    }
+  }
+
+  /// 회원가입
+  Future<DefaultResponse?> register(PostRegisterRequest request) async {
+    try {
+      var response = await _authApi.register(request);
+      return response;
+    } catch (e) {
+      logger.e("Failed to register.", error: e);
 
       if (e is DioException && e.response != null) {
         return null;
